5f6ce4eb4525df648a5ec6aaa0436f5127f30cda
German Carosio <carosiogerman@gmail.com>
2022-04-25 11:52:30 -0300

    se agrego app.get en app.js para login y register

--- app.js
+++ app.js
@@ -14,3 +14,11 @@ app.get("/", (req,res)=>{
 app.get("/productDetail", (req,res)=>{
   res.sendFile(__dirname + "/views/productDetail.html")
 })
+
+app.get("/login", (req,res)=>{
+  res.sendFile(__dirname + "/views/login.html")
+})
+
+app.get("/register", (req,res)=>{
+  res.sendFile(__dirname + "/views/register.html")
+})
