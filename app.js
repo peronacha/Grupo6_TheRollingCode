1a695f79166ae4cd60f19239d868e7b1075eed0a
German Carosio <GCAROSIO@claro.com.ar>
2022-04-26 12:30:53 -0300

    se agrego el app.get de productCart.html

--- app.js
+++ app.js
@@ -23,3 +23,7 @@ app.get("/login", (req,res)=>{
 app.get("/register", (req,res)=>{
   res.sendFile(__dirname + "/views/register.html")
 })
+
+app.get("/register", (req,res)=>{
+  res.sendFile(__dirname + "/views/productCart.html")
+})
